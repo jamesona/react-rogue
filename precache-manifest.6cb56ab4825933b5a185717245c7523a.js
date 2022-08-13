32051e05ba71f634f96317b2d536c2e1de14d1e2
Jameson Aranda <jamesonaranda@gmail.com>
2022-08-13 12:31:49 -0600

    update

---
+++ precache-manifest.6cb56ab4825933b5a185717245c7523a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a5755da4e3ea8c057ddcb598f2c6a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de487e73551398381d32",
+    "url": "/static/css/main.9cc79735.chunk.css"
+  },
+  {
+    "revision": "13323b39f9e68c8afb88",
+    "url": "/static/js/2.5aa735ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5aa735ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de487e73551398381d32",
+    "url": "/static/js/main.ef9d5c45.chunk.js"
+  },
+  {
+    "revision": "2bf6ab173284d891031f",
+    "url": "/static/js/runtime-main.fff218e8.js"
+  }
+]);
