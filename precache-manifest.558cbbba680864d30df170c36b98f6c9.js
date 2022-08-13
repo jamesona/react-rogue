3c0e5dfce0454da1b95c05299849acce4e0240da
jamesona <jamesona@users.noreply.github.com>
2022-08-13 18:58:10 +0000

    Build: (a1aeb56) Merge branch 'master' of github.com:jamesona/react-rogue

---
+++ precache-manifest.558cbbba680864d30df170c36b98f6c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d11df02f551f8bbd6d1e258aa0a2bb7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7fb2dc36a0c3866cfd9e",
+    "url": "./static/css/main.8be73394.chunk.css"
+  },
+  {
+    "revision": "6ff5132a721281c068b0",
+    "url": "./static/js/2.c3bfaf8c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.c3bfaf8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fb2dc36a0c3866cfd9e",
+    "url": "./static/js/main.592cffff.chunk.js"
+  },
+  {
+    "revision": "76b10b1321109c94ed87",
+    "url": "./static/js/runtime-main.4f1d6680.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9419ca0bfb0c196cc276c4211f305cf0.js"
+  "./precache-manifest.558cbbba680864d30df170c36b98f6c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
